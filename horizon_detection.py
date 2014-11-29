750a2b709df13f9fb8f2d21cb3ecb35a7e734734
derek-lun <dereklun@gmail.com>
2014-11-29 12:11:13 -0800

    added try/except for robustness

--- horizon_detection.py
+++ horizon_detection.py
@@ -14,13 +14,16 @@ for file in os.listdir(os.path.dirname(sys.argv[0])):
 		lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, None, 30, 12);
 		longest_line = 0
 		longest_points = ()
-		for line in lines[0]:
-			pt1 = (line[0],line[1])
-			pt2 = (line[2],line[3])
-			current_line = ((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)
-			if (current_line > longest_line):
-				longest_line = current_line
-				longest_points = (pt1, pt2)
-		if (longest_line > 0):		
-			cv2.line(img, longest_points[0], longest_points[1], (0,0,255), 3)
-		cv2.imwrite(str(os.path.splitext(file)[0]) + " - horizon.jpg", img)
+		try:
+			for line in lines[0]:
+				pt1 = (line[0],line[1])
+				pt2 = (line[2],line[3])
+				current_line = ((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)
+				if (current_line > longest_line):
+					longest_line = current_line
+					longest_points = (pt1, pt2)
+			if (longest_line > 0):		
+				cv2.line(img, longest_points[0], longest_points[1], (0,0,255), 3)
+			cv2.imwrite(str(os.path.splitext(file)[0]) + " - horizon.jpg", img)
+		except:
+			pass
