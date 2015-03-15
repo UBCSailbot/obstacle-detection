ebd46540191dc6c1a464fe714f62f6081487bf5c
Arek Sredzki <arek@vikom.io>
2015-03-14 19:31:00 -0700

    Improved scaling

--- rpi_lepton_video_rewrite/main.cpp
+++ rpi_lepton_video_rewrite/main.cpp
@@ -1,11 +1,19 @@
 
-#include <opencv2/core/core.hpp>
 #include "Lepton.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+
+#define APP_NAME "Lepton Capture"
+#define WIN_ZOOM_FACTOR 8
+#define WIN_WIDTH 80 * WIN_ZOOM_FACTOR
+#define WIN_HEIGHT 60 * WIN_ZOOM_FACTOR
+#define EIGHT_BITS 256
 
 int main() {
     Lepton lepton;
@@ -14,19 +22,30 @@ int main() {
     bool trackingEnabled = false;
     bool debugModeEnabled = false;
     cv::Mat frame;
+    cv::Mat eightBit;
 
-    const string windowName = "Capture";
 
     while (1) {
 
         frame = lepton.GetFrame();
 
         // We must convert the frame to 8 bit display it properly
-        cv::Mat eightBit(240, 180, CV_8UC1);
-        frame.convertTo(eightBit, CV_8UC1);
-        cv::resize(eightBit, eightBit, eightBit.size(), 3, 3, cv::INTER_NEAREST);
 
-        imshow(windowName, eightBit);
+        double minVal_orig;
+        double maxVal_orig;
+        cv::minMaxLoc(frame, &minVal_orig, &maxVal_orig);
+        cv::Mat displayed(WIN_HEIGHT, WIN_WIDTH, CV_8UC1);
+
+        int range = maxVal_orig - minVal_orig;
+
+        int minVal_trackbar = 128;
+        int maxVal_trackbar = range + 128;
+
+        // Convert input image to 8-bit and resize
+        frame.convertTo(eightBit, CV_8UC1, 255.0/(maxVal_orig - minVal_orig), -minVal_orig * 255.0/(maxVal_orig - minVal_orig));
+        cv::resize(eightBit, displayed, displayed.size(), 0, 0, cv::INTER_NEAREST);
+
+        imshow(APP_NAME, displayed);
 
         // Check to see if a button has been pressed.
         // this 10ms delay is necessary for proper operation of this program
