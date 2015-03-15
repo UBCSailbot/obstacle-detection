3e948feec1f3c0a7e29a9e9aea23d0aa79981dba
Paul Cernek <ptcernek@gmail.com>
2015-03-15 00:47:52 -0700

    Video histogram code usable for walking directory tree

--- thermoscale/run_pixel_histo.py
+++ thermoscale/run_pixel_histo.py
@@ -1,1 +1,8 @@
 import os
+import pixel_histo as ph
+
+top_dir = "/home/paul/sailbot/expt/20150219_VIGIL/data"
+
+for cur_dir, dirnames, f in os.walk(top_dir):
+	if cur_dir.endswith("raw"):
+		ph.generate_video_histo(cur_dir)
