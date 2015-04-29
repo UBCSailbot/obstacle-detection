9ae8e76c214b296cd21f4c7e9db4a57547beb78d
Paul Cernek <ptcernek@gmail.com>
2015-04-28 19:33:58 -0700

    Intermediate stage: hooked up quaternions to test_horizon via ImuProcessor, about to clean up

--- imu/imu_processor.h
+++ imu/imu_processor.h
@@ -2,38 +2,46 @@
 #define IMU_PROCESSOR_H
 
 #include "imu_data.h" // TODO
+#define betaDef		0.1f
 
 typedef std::chrono::duration<std::chrono::steady_clock> TimeDuration;
+
+typedef struct Quaternion {
+	float q0;
+	float q1;
+	float q2;
+	float q3;
+} Quaternion;
 
 // TODO: Finish header file
 class ImuProcessor
 {
 public:
 
-	ImuProcessor(const ImuData& data);
+	ImuProcessor(const ImuData& data,
+		float initRoll=0, float initPitch=0, float initYaw=0);
 
 	float getRoll() const;
 	float getPitch() const;
+	float getYaw() const;
 
 	void update(const ImuData& data);
-	void resetGyro(const ImuData& data);
 
 private:
 	int getTimeStepInMilliseconds(const ImuData& data);
-	void updateAccelAverage(const ImuData& data, int timeStepInMilliseconds);
-	void updatePitchAndRoll(const ImuData& data, int timeStepInMilliseconds);
+	void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, int timeStep);
+	void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, int timeStep);
+	float invSqrt(float x);
 
-	float getAccelRoll(const ImuData& data) const;
-	float getAccelPitch(const ImuData& data) const;
+	float computeRoll(Quaternion quaternion);
+	float computePitch(Quaternion quaternion);
+	float computeYaw(Quaternion quaternion);
 
 	float currentRoll;
 	float currentPitch;
+	float currentYaw;
 
-	float accelRoll;
-	float accelPitch;
-
-	int timeSinceLastReset;
-	int readingsSinceLastReset;
+	Quaternion quaternion;
 
 	TimePoint lastTimestamp;
 
