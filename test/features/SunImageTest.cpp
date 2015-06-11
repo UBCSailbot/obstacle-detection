1291ab3843e3a8fb9abebc9ef7d7e79336dbaaf7
delun <dereklun@gmail.com>
2015-06-11 02:47:12 -0700

    Fixed math in test with offset

--- test/features/SunImageTest.cpp
+++ test/features/SunImageTest.cpp
@@ -28,7 +28,7 @@ TEST_F(SunImageTest, testMinSunPixelValue) {
 
 TEST_F(SunImageTest, test1PixFlatPointFlatHorizon) {
     cv::Mat zeros = cv::Mat(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX, CV_16UC1, cv::Scalar(0));
-    zeros.at<uint16_t>(20, 0) = 9000;
+    zeros.at<uint16_t>(0, 20) = 9000;
     Horizon h = Horizon(cv::Point2f (0, 0), cv::Point2f (VIEWPORT_WIDTH_PIX - 1, 0));
     SunImage sunImage = SunImage(h, zeros, 9000, 1);
 
@@ -55,6 +55,17 @@ TEST_F(SunImageTest, test1PixFlatPointDiagHorizon) {
     SunImage sunImage = SunImage(h, zeros, 9000, 1);
 
     sunImage.findSunColumn();
-    EXPECT_FLOAT_EQ(sunImage.getMean(), 20*sin(atan(59.0/79.0)));
+    EXPECT_FLOAT_EQ(sunImage.getMean(), 20*cos(atan(59.0/79.0)));
+    EXPECT_EQ(sunImage.getVariance(), 0);
+}
+
+TEST_F(SunImageTest, test1PixFlatPointDiagHorizonWithOffSet) {
+    cv::Mat zeros = cv::Mat(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX, CV_16UC1, cv::Scalar(0));
+    zeros.at<uint16_t>(0, 20) = 9000;
+    Horizon h = Horizon(cv::Point2f (0, 59), cv::Point2f (VIEWPORT_WIDTH_PIX - 1, 0));
+    SunImage sunImage = SunImage(h, zeros, 9000, 1);
+
+    sunImage.findSunColumn();
+    EXPECT_FLOAT_EQ(sunImage.getMean(), pow(pow(20, 2) + pow(59, 2), 0.5)*cos(atan(59.0/20.0) - atan(59.0/79.0)));
     EXPECT_EQ(sunImage.getVariance(), 0);
 }
