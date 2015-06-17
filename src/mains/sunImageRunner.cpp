bc726e1a7a7b006204c5fb4be40f956bdd94617e
delun <dereklun@gmail.com>
2015-06-17 16:43:35 -0700

    added color to runner

--- src/mains/sunImageRunner.cpp
+++ src/mains/sunImageRunner.cpp
@@ -6,18 +6,19 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    cv::Mat frame = cv::imread("resources/img/freighterAndSun01.png", CV_LOAD_IMAGE_UNCHANGED);
+    cv::Mat grayScale = cv::imread("resources/img/freighterAndSun01.png", CV_LOAD_IMAGE_UNCHANGED);
+    cv::Mat color = cv::imread("resources/img/freighterAndSun01.png", CV_LOAD_IMAGE_COLOR);
     Horizon h = Horizon(cv::Point2f (0, 50), cv::Point2f (VIEWPORT_WIDTH_PIX - 1, 50));
-    SunImage sunImage = SunImage(h, frame, 8150, 1);
+    SunImage sunImage = SunImage(h, grayScale, 9000, 1);
     sunImage.findPosition();
     sunImage.findColumn();
 
-    line(frame, sunImage.getPosition(), sunImage.getPosition(), 0);
-    line(frame, h.getStartPoint(), h.getEndPoint(), 0);
-    line(frame, sunImage.getLeftMargin().getStartPoint(), sunImage.getLeftMargin().getEndPoint(), 0);
-    line(frame, sunImage.getRightMargin().getStartPoint(), sunImage.getRightMargin().getEndPoint(), 0);
+    line(color, sunImage.getPosition(), sunImage.getPosition(), cv::Scalar(0, 0, 0xFFFF));
+    line(color, h.getStartPoint(), h.getEndPoint(), cv::Scalar(0xFFFF, 0, 0));
+    line(color, sunImage.getLeftMargin().getStartPoint(), sunImage.getLeftMargin().getEndPoint(), cv::Scalar(0, 0xFFFF, 0));
+    line(color, sunImage.getRightMargin().getStartPoint(), sunImage.getRightMargin().getEndPoint(), cv::Scalar(0, 0xFFFF, 0));
 
-    resize(frame, frame, cv::Size(400,300), 0, 0, cv::INTER_NEAREST);
-    cv::imshow("windows", frame);
+    resize(color, color, cv::Size(400,300), 0, 0, cv::INTER_NEAREST);
+    cv::imshow("windows", color);
     cv::waitKey(0);
 }
