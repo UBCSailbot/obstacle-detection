4648cd7036dfb175c7a87fcb49c78be5684edeab
delun <dereklun@gmail.com>
2015-06-18 17:03:03 -0700

    added parse input

--- src/mains/sunImageRunner.cpp
+++ src/mains/sunImageRunner.cpp
@@ -6,10 +6,24 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    cv::Mat grayScale = cv::imread("resources/img/freighterAndSun01.png", CV_LOAD_IMAGE_UNCHANGED);
-    cv::Mat color = cv::imread("resources/img/freighterAndSun01.png", CV_LOAD_IMAGE_COLOR);
+
+
+    if(argc < 4) {
+        std::cout << "Usage: <input_image> <sun_brightness> <margin>" << std::endl;
+        return -1;
+    }
+
+    cv::Mat grayScale = cv::imread(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+    cv::Mat color = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+    if(! grayScale.data )
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
     Horizon h = Horizon(cv::Point2f (0, 50), cv::Point2f (VIEWPORT_WIDTH_PIX - 1, 50));
-    SunImage sunImage = SunImage(h, grayScale, 9000, 1);
+    SunImage sunImage = SunImage(h, grayScale, atoi(argv[2]), atof(argv[3]));
     sunImage.findPosition();
     sunImage.findColumn();
 
