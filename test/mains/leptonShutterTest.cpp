942392cef08b8cb611cc1cb93f8c0821dfd820a5
Aditi Chakravarthi <aditichak@hotmail.com>
2015-06-20 19:11:49 -0700

    lepton shutter functions

---
+++ test/mains/leptonShutterTest.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Aditi Chakravarthi on 15-06-20.
+//
+
+#include "lepton/leptonI2C.h"
+
+
+
+
+int main() {
+
+void lepton_openShutter();
+
+    
+
+void lepton_closeShutter();
+
+
+}
+
+
+
+
