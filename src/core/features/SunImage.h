353733342533d054b8f92e187340474f870c549d
Paul Cernek <ptcernek@gmail.com>
2015-06-25 00:20:01 -0700

    add documentation

--- src/core/features/SunImage.h
+++ src/core/features/SunImage.h
@@ -13,6 +13,22 @@
 #include "types/Rect2f.h"
 #include "lepton/LeptonCameraDimensions.h"
 
+/**
+ * Uses a simple heuristic to determine the parts of an image that are likely
+ *  to contain sun glint.
+ *
+ * The heuristic works as follows:
+ *      - First, we check if the sun is in the frame by searching for pixels
+ *        above a given threshold value.
+ *      - If the sun is found in the frame, then two lines perpendicular to
+ *        the horizon in that frame are projected from either side of the sun,
+ *        with a given margin.
+ *      - If the sun is not found in the frame, then a second, lower threshold
+ *        value is used to search for pixels below the horizon that might be
+ *        reflections of the sun in the water. In similar fashion two lines
+ *        are found perpendicular to the horizon that delimit a column in
+ *        which higher-intensity pixels are likely to be sun glint.
+ */
 class SunImage
 {
 public:
@@ -23,17 +39,48 @@ public:
     ~SunImage();
 
     void findSunPosition();
-    bool containsSun() const;
+
+    /**
+     * RETURN: The bounding box around the sun in the image, if the sun is
+     *  present. Otherwise, returns a rectangle whose upper left corner is
+     *  the point (-1,-1) and whose dimensions are -1 x -1.
+     */
     Rect2f getSunPosition() const;
 
+    /**
+     * RETURN: Whether or not the sun is visible within the frame.
+     */
+    bool containsSun() const;
+
     void findGlintColumn();
-    Line*getLeftGlintMargin() const;
-    Line*getRightGlintMargin() const;
+
+    /**
+     * RETURN: The left margin of the column containing sun glint in
+     *  the frame.
+     */
+    Line* getLeftGlintMargin() const;
+
+    /**
+     * RETURN: The right margin of the column containing sun glint in
+     *  the frame.
+     */
+    Line* getRightGlintMargin() const;
 
     void findMeanVariance();
     float getMean() const;
     float getVariance() const;
 
+    /**
+     * PRE: The position of the sun in the image, or its absence therefrom,
+     *  has been ascertained.
+     * PRE: A column perpendicular to the horizon has been found within which
+     *  sun glint is likely to occur.
+     *
+     * RETURN: An 8-bit 3-channel color Mat with the following features
+     *  rendered on it:
+     *      - a red rectangle around the sun, if the sun is in the frame
+     *      - a
+     */
     cv::Mat render() const;
 
 private:
