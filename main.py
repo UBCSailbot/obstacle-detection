440af529699f3c5b54bb9dcf8c5062d15eb3051e
Derek-Lun <dereklun@gmail.com>
2015-06-29 13:21:18 -0700

    Added read video file from arguments

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import cv2
 import copy
+import sys
+
+ROIs = set()
+start_position = None
 
 def on_mouse(event, x, y, flags, frame):
     frameRect = copy.copy(frame)
@@ -21,47 +25,53 @@ def on_mouse(event, x, y, flags, frame):
         ROIs.clear()
         cv2.imshow('frame',frameRect)
 
-cap = cv2.VideoCapture('drop.avi')
-ROIs = set()
-start_position = (0, 0)
+def main(argv):
+    if len(sys.argv) != 2:
+        print "Usage: main.py <file name>"
+        sys.exit()
 
-if not cap.isOpened():
-  print "Error when reading image file"
+    cap = cv2.VideoCapture(sys.argv[1])
 
-while(cap.isOpened()):
-    ROIs.clear()
-    ret, frame = cap.read()
+    if not cap.isOpened():
+      print "Error when reading image file"
 
-    cv2.putText(frame,
-    str(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)),
-    (25, 50),
-    cv2.FONT_HERSHEY_SIMPLEX,
-    2,
-    (255, 0, 0))
+    while(cap.isOpened()):
+        ROIs.clear()
+        ret, frame = cap.read()
 
-    cv2.namedWindow('frame')
-    cv2.cv.SetMouseCallback('frame', on_mouse, frame)
-    cv2.imshow('frame',frame)
+        cv2.putText(frame,
+        str(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)),
+        (25, 50),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        2,
+        (255, 0, 0))
 
-    k=-1
-    while k not in (27, ord('u'), ord('n'), ord('s'), 2490368, 2621440, 2555904):
-        k = cv2.waitKey(0)
-        print str(k) + " was pressed"
+        cv2.namedWindow('frame')
+        cv2.cv.SetMouseCallback('frame', on_mouse, frame)
+        cv2.imshow('frame',frame)
 
-    if k==27:    # Esc key to stop
-        break
-    elif k==ord('u'):
-        print "undefined"
-        continue
-    elif k==ord('n'):
-        print "nothing interesting"
-        continue
-    elif k==2490368:
-        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) - 25)
-    elif k==2621440:
-        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) + 23)
-    elif k==2555904:  # right arrow for next frame
-        continue
+        k=-1
+        while k not in (27, ord('u'), ord('n'), ord('s'), 2490368, 2621440, 2555904):
+            k = cv2.waitKey(0)
+            print str(k) + " was pressed"
 
-cap.release()
-cv2.destroyAllWindows()
+        if k==27:    # Esc key to stop
+            break
+        elif k==ord('u'):
+            print "undefined"
+            continue
+        elif k==ord('n'):
+            print "nothing interesting"
+            continue
+        elif k==2490368: # up arrow for fast reverse
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) - 25)
+        elif k==2621440: # down arrow for fast forward
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) + 23)
+        elif k==2555904: # right arrow for next frame
+            continue
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
