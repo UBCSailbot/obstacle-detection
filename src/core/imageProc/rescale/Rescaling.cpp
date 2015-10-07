62bbd3895d7dedebe2ac446dff77be65204f4733
Paul Cernek <ptcernek@gmail.com>
2015-10-06 18:35:40 -0700

    change default rescaling algorithm to use a 512-pixel-value-window instead of 256.
    Thus all pixel values are either clipped, or lumped in with a neighboring value.

--- src/core/imageProc/rescale/Rescaling.cpp
+++ src/core/imageProc/rescale/Rescaling.cpp
@@ -9,12 +9,12 @@ void Rescaling::clipTo8bits(const Image16bit &src, Image8bit &dst, const int &ne
 
     for (int row = 0; row < src.rows; row++) {
         for (int col = 0; col < src.cols; col++) {
-            int scaledValue = src.at<uint16_t>(row, col) - newMidPixelValue + 127;
-            if (scaledValue > 255)
-                scaledValue = 255;
+            int scaledValue = src.at<uint16_t>(row, col) - newMidPixelValue + 255;
+            if (scaledValue > 511)
+                scaledValue = 511;
             else if (scaledValue < 0)
                 scaledValue = 0;
-            dst.at<uint8_t>(row, col) = (uint8_t) scaledValue;
+            dst.at<uint8_t>(row, col) = (uint8_t) (scaledValue / 2);
         }
     }
 
