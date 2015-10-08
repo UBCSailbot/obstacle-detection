a1ac729c9c332e6ee713c25b225dacc80d7fdb4a
Paul Cernek <ptcernek@gmail.com>
2015-10-07 23:29:43 -0700

    finish process_experiment.py script, and make mostly aesthetic adjustments to others

--- scripts/process_experiment.py
+++ scripts/process_experiment.py
@@ -11,24 +11,71 @@ Uses a config file to specify what to do for every capture directory within the 
 """
 
 import adjust_imu_logfile
-import rescale_multiple
+import rescale
+import make_video
 import shared
 
-import sys
+import sys, os
+from shutil import rmtree
+
+from ast import literal_eval
+
+class Config:
+	pass
+
+def config_or_default(config_dict, field_name, default_value):
+	try:
+		return config_dict[field_name]
+	except:
+		return default_value
+
+def parse_boolean(bool_string):
+	return literal_eval(bool_string.capitalize())
 
 def parse_config(config_file):
+	config_dict = { line.strip().split('=')[0] : line.strip().split('=')[1] for line in open(config_file, 'r').readlines() if not line.isspace() and not line.startswith('#') }
+
+	config = Config()
+
+	config.roll_offset = float(config_or_default(config_dict, 'roll-offset', 0))
+	config.pitch_offset = float(config_or_default(config_dict, 'pitch-offset', 0))
+	config.invert_roll = parse_boolean(config_or_default(config_dict, 'invert-roll', False))
+	config.invert_pitch = parse_boolean(config_or_default(config_dict, 'invert-pitch', False))
+
+	# rescaling options
+	rescaling_type = int(config_or_default(config_dict, 'rescaling-type', 3))
+	smoothing_window = int(config_or_default(config_dict, 'smoothing-window', 1))
+	config.rescaling_options = "--rescaling-type {0} --smoothing-window {1}".format(rescaling_type, smoothing_window)
+	config.rescaling_desc = '_'.join([rescale.rescaling_types[rescaling_type - 1], 'w' + str(smoothing_window) ])
+
+	# video options
+	config.frame_rate = int(config_or_default(config_dict, 'framerate-fps', 9))
+	config.rotate_frames = parse_boolean(config_or_default(config_dict, 'rotate-frames', False))
+
+	return config
 
 
-def adjust_imu_logfiles(experiment_dir, config):
-	adjust_imu_logfile.adjust_imu_logfiles(experiment_dir, config.roll_offset, config.pitch_offset, invert_roll=config.invert_roll, invert_pitch=config.invert_pitch)
+def adjust_imu_logfile_wrapper(capture_dir, config):
+	logfile_path = os.path.join(capture_dir, shared.default_imu_output_file)
+	adjust_imu_logfile.adjust_imu_logfile(logfile_path, config.roll_offset, config.pitch_offset, invert_roll=config.invert_roll, invert_pitch=config.invert_pitch)
 
-def rescale_multiple(experiment_dir, config):
-	rescale_multiple.rescale_multiple(experiment_dir, horizon_file_name=shared.default_imu_output_file_adjusted, options=shared.conditional_concat(config.rescaling_options, " --paint-horizon"), description=config.rescaling_desc)
+def rescale_wrapper(capture_dir, rescaled_dir_name, config, paint_horizon=False):
+	raw_dir = os.path.join(capture_dir, 'raw')
+	output_dir = os.path.join(capture_dir, rescaled_dir_name)
+	orientation_file = os.path.join(capture_dir, shared.default_imu_output_file_adjusted)
 
+	rescaling_options = config.rescaling_options
 
-def delete_temp_rescaled_frames(experiment_dir, config):
-	for root, dirs, files in os.walk(experiment_dir):
-		if config.rescaling_ in dirs:
+	if paint_horizon:
+		rescaling_options = shared.conditional_concat(rescaling_options, " --paint-horizon")
+
+	rescale.rescale(raw_dir, output_dir, options=rescaling_options, orientation_file=orientation_file)
+
+def make_video_wrapper(capture_dir, config):
+	frame_dir = os.path.join(capture_dir, 'temp')
+	video_file = os.path.join(capture_dir, config.rescaling_desc + '.avi')
+
+	make_video.convert_to_video(frame_dir, video_file, config.frame_rate, rotate=config.rotate_frames)
 
 
 def main(config_file):
@@ -36,32 +83,26 @@ def main(config_file):
 
 	top_dir = os.path.dirname(config_file)
 
-	# 1. adjust imu logfiles	
-	adjust_imu_logfiles(top_dir, config)
+	for root, dirs, files in os.walk(top_dir):
+		if "raw" in dirs:  # indicates that the current directory contains a directory of raw frames
 
-	#2. rescale with horizon painted on & 3. make videos
-	rescale_multiple(top_dir, config)
+			# 1. adjust imu logfile
+			adjust_imu_logfile_wrapper(root, config)
 
-	# 4. batch delete
-	delete_temp_rescaled_frames(top_dir, config)
+			#2. rescale with horizon painted on
+			rescale_wrapper(root, 'temp', config, paint_horizon=True)
 
-	# 5. batch rescale again, without horizon
+			# 3. make video
+			make_video_wrapper(root, config)
 
+			# 4. batch delete
+			rmtree( os.path.join(root, 'temp') )
 
-
-	for capture_dir in [os.path.join(top_dir, x) for x in os.listdir(top_dir) if os.path.isdir(x)]:
-		
-		new_imu_logfile = adjust_imu_logfile(capture_dir, config)
-
-		raw_dir = os.path.join(top_dir, capture_dir)
-		rescaled_dir = 
-
-
-
-
+			# 5. rescale again, without horizon
+			rescale_wrapper(root, 'rescaled_' + config.rescaling_desc, config)
 
 
 if __name__ == "__main__":
-	config_file = sys.argv[2]
+	config_file = sys.argv[1]
 
 	main(config_file)
