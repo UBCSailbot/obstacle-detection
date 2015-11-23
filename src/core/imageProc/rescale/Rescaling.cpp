4764558ba59cc32ad9e88bd85b437e70002ac047
Paul Cernek <ptcernek@gmail.com>
2015-11-22 23:31:03 -0800

    Restore clipping to be 1:1, from 2:1 which it was previously.
    1:1 is what we've used for most testing in the past. The latest dataset uploaded via Dropbox was produced using 2:1. This provided some benefits by expanding the range of temperatures we can detect, but also the drawback of decreased contrast due to compression of objects close to water temp. Further work remains to be done here, so for now we're reverting to the default of no compression.

--- src/core/imageProc/rescale/Rescaling.cpp
+++ src/core/imageProc/rescale/Rescaling.cpp
@@ -4,7 +4,7 @@
 
 #include "Rescaling.h"
 
-void Rescaling::clipTo8bits(const Image16bit &src, Image8bit &dst, const int &newMidPixelValue) {
+void Rescaling::clipTo8bitsCompress2to1(const Image16bit &src, Image8bit &dst, const int &newMidPixelValue) {
     assert(dst.size == src.size);
 
     for (int row = 0; row < src.rows; row++) {
@@ -15,6 +15,22 @@ void Rescaling::clipTo8bits(const Image16bit &src, Image8bit &dst, const int &ne
             else if (scaledValue < 0)
                 scaledValue = 0;
             dst.at<uint8_t>(row, col) = (uint8_t) (scaledValue / 2);
+        }
+    }
+
+}
+
+void Rescaling::clipTo8bitsNoCompression(const Image16bit &src, Image8bit &dst, const int &newMidPixelValue) {
+    assert(dst.size == src.size);
+
+    for (int row = 0; row < src.rows; row++) {
+        for (int col = 0; col < src.cols; col++) {
+            int scaledValue = src.at<uint16_t>(row, col) - newMidPixelValue + 127;
+            if (scaledValue > 255)
+                scaledValue = 255;
+            else if (scaledValue < 0)
+                scaledValue = 0;
+            dst.at<uint8_t>(row, col) = (uint8_t) scaledValue;
         }
     }
 
