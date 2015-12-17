946707ed03b8bdac6d081106cc1444db6e317f00
Paul Cernek <ptcernek@gmail.com>
2015-12-17 15:52:14 -0800

    fix failed tests by updating with exceptions

--- test/core/imageProc/rescale/RescalingTest.cpp
+++ test/core/imageProc/rescale/RescalingTest.cpp
@@ -2,13 +2,14 @@
 // Created by paul on 09/06/15.
 //
 
+#include <exceptions/ImageSizeMismatchException.h>
 #include "RescalingTest.h"
 
 TEST_F(RescalingTest, eightBitWindowSizeAssertion) {
     Image16bit src(5,6);
     Image8bit dst(4,5);
     int newMinPixelValue = 1;
-    EXPECT_DEATH(Rescaling::clipTo8bitsCompress2to1(src, dst, newMinPixelValue), "c*");
+    EXPECT_THROW(Rescaling::clipTo8bitsCompress2to1(src, dst, newMinPixelValue), ImageSizeMismatchException);
 }
 
 TEST_F(RescalingTest, testAllValuesNoCompression) {
