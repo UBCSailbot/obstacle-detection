c596021a1cca9219d971545c2e0dabd8ad712803
Paul Cernek <ptcernek@gmail.com>
2016-01-20 17:22:26 -0800

    add a main function and support test Lepton class

---
+++ test/device/lepton/RedundantLepton.cpp
@@ -0,0 +1,110 @@
+//
+// Created by paul on 20/01/16.
+//
+
+#include <lepton/LeptonSPI.h>
+#include <lepton/Lepton.h>
+#include "RedundantLepton.h"
+
+RedundantLepton::RedundantLepton(int spiChipSelect) {
+    SpiOpenPort(spiChipSelect);
+
+    int ret = 0;
+    int fd;
+
+    sprintf(_device, "/dev/spidev%d.0", spiChipSelect);
+
+    fd = open(_device, O_RDWR);
+    if (fd < 0)
+    {
+        pabort("can't open device");
+    }
+
+    ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    if (ret == -1)
+    {
+        pabort("can't set spi mode");
+    }
+
+    ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+    if (ret == -1)
+    {
+        pabort("can't get spi mode");
+    }
+
+    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    if (ret == -1)
+    {
+        pabort("can't set bits per word");
+    }
+
+    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+    if (ret == -1)
+    {
+        pabort("can't get bits per word");
+    }
+
+    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    if (ret == -1)
+    {
+        pabort("can't set max speed hz");
+    }
+
+    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+    if (ret == -1)
+    {
+        pabort("can't get max speed hz");
+    }
+
+}
+
+RedundantLepton::~RedundantLepton() {
+    SpiClosePort(0);
+}
+
+void RedundantLepton::getFrame(Image16bit &frame) {
+    //read data packets from lepton over SPI
+    int resets = 0;
+    for (int j = 0; j < PACKETS_PER_FRAME; j++) {
+        //if it's a drop packet, reset j to 0, set to -1 so he'll be at 0 again loop
+        read(spi_cs0_fd, _result + sizeof(uint8_t) * PACKET_SIZE * j, sizeof(uint8_t) * PACKET_SIZE);
+        int packetNumber = _result[j * PACKET_SIZE + 1];
+        if (packetNumber != j) {
+            j = -1;
+            resets += 1;
+            usleep(1000);
+            if (resets == 750) {
+                SpiClosePort(0);
+                usleep(750000);
+                SpiOpenPort(0);
+            }
+        }
+    }
+
+    if (resets >= 30) {
+        std::cout << "Done reading, resets: " << resets << std::endl;
+    }
+
+    _frameBuffer = (uint16_t *) _result;
+
+    for (int i = 0; i < FRAME_SIZE_UINT16; i++) {
+        //skip the first 2 uint16_t's of every packet, they're 4 header bytes
+        if (i % PACKET_SIZE_UINT16 < 2) {
+            continue;
+        }
+
+        //flip the MSB and LSB at the last second
+        uint8_t temp = _result[i * 2];
+        _result[i * 2] = _result[i * 2 + 1];
+        _result[i * 2 + 1] = temp;
+
+    }
+
+    for(int row=0; row < frame.rows; row++) {
+        for (int col=0; col < frame.cols; col++) {
+            frame.pixelAt(row, col) = _frameBuffer[row * VIEWPORT_WIDTH_PIX + col];
+        }
+    }
+
+
+}
