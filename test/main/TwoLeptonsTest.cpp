a980995d1cc782a8418c3bb3c87585f46a6bb3b5
Paul Cernek <ptcernek@gmail.com>
2016-01-25 01:50:59 -0800

    Refine TwoLeptonsTest

--- test/main/TwoLeptonsTest.cpp
+++ test/main/TwoLeptonsTest.cpp
@@ -14,58 +14,93 @@
 
 using namespace std;
 
+void run_single(int lepton_cs) {
 
-
-void run() {
-
-    RedundantLepton lepton0(0);
-    //Lepton lepton1(1);
+    RedundantLepton lepton0(lepton_cs);
     SimpleRescaler rescaler;
 
-    Image16bit frame(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX);
-    Image8bit displayed(60, 80);
+    Image16bit frame0(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX);
+    Image8bit displayed0(60, 80);
     int frame_counter = 1;
-
-    // timing
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    float leptonPeriodSeconds = 1 / LEPTON_FPS;
 
     cout << "Connecting to screen" << endl;
 
-    Display* display = DisplayUtils::connectToDisplay();
+    Display* display0 = new DesktopDisplay("Lepton0"); 
 
     cout << "Starting Capture" << endl;
 
     while (1) {
 
-        start = std::chrono::system_clock::now();
-
         // read only every 3rd frame
         if ((frame_counter % 3) == 0) {
-            lepton0.getFrame(frame);
+            lepton0.getFrame(frame0);
+            cout << "lepton " << lepton_cs << " got frame " << frame_counter << endl; 
 
             // convert to 8 bit and display
-            rescaler.scale16bitTo8bit(frame, displayed);
-            display->display8bitGray(displayed);
+            rescaler.scale16bitTo8bit(frame0, displayed0);
+
+            display0->display8bitGray(displayed0);
+
+//            cout << "now displaying. can you see it?" << endl;
         }
 
         frame_counter ++;
 
-        end = std::chrono::system_clock::now();
-        std::chrono::duration<float> elapsed_seconds = end-start;
+    }
 
-        if (elapsed_seconds.count() < leptonPeriodSeconds) {
-            unsigned int sleepTimeMicros = (unsigned int) (leptonPeriodSeconds - elapsed_seconds.count()) * 1000000;
-            usleep(sleepTimeMicros);
-        }
+}
+
+void run_double() {
+
+    RedundantLepton lepton0(0);
+    RedundantLepton lepton1(1);
+    SimpleRescaler rescaler;
+
+    Image16bit frame0(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX);
+    Image16bit frame1(VIEWPORT_HEIGHT_PIX, VIEWPORT_WIDTH_PIX);
+    Image8bit displayed0(60, 80);
+    Image8bit displayed1(60, 80);
+    int frame_counter = 1;
+
+    cout << "Connecting to screen" << endl;
+
+    Display* display0 = new DesktopDisplay("Lepton0"); 
+    Display* display1 = new DesktopDisplay("Lepton1"); 
+
+    cout << "Starting Capture" << endl;
+
+    while (1) {
+
+        // read only every 3rd frame
+        //if ((frame_counter % 3) == 0) {
+            lepton0.getFrame(frame0);
+            //cout << "lepton0 got frame " << frame_counter << endl; 
+
+            lepton1.getFrame(frame1);
+            //cout << "lepton1 got frame " << frame_counter << endl; 
+
+            // convert to 8 bit and display
+            rescaler.scale16bitTo8bit(frame0, displayed0);
+            rescaler.scale16bitTo8bit(frame1, displayed1);
+
+            display0->display8bitGray(displayed0);
+            display1->display8bitGray(displayed1);
+
+//            cout << "now displaying. can you see it?" << endl;
+        //}
+
+        frame_counter ++;
 
     }
 
 }
 
 int main(int argc, char** argv) {
-
-    run();
+    if(argc < 2) {
+        run_double();
+    } 
+    int lepton_cs = atoi(argv[1]);
+    run_single(lepton_cs);
 
     return 0;
 }
