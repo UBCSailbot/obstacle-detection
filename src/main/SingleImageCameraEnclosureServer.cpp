98d3a1da447437aae84328af4ef292c833876ef3
Denis Trailin <denis.trailin@gmail.com>
2016-06-05 11:36:22 -0700

    brought back old TCPImageStream compatable server

---
+++ src/main/SingleImageCameraEnclosureServer.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <comm/TCPImageServer.h>
+#include <paths/Paths.h>
+#include <paths/Resources.h>
+#include <io/FileSystemImageStream.h>
+#include <camera/lepton/ThermalCameraStream.h>
+#include <camera/lepton/LeptonRegistry.h>
+
+#define APPNAME "cameraServer"
+
+/**
+ * Spins up an instance of a server that listens for client requests for
+ *  16bit grayscale images, and replies by sending images drawn from a
+ *  stream. In DEBUG mode, sends test images saved in a local directory;
+ *  otherwise, attempts to connect to the specified Lepton and streams
+ *  ouptut from that device.
+ *
+ * For help on understanding this program, please see the usage notes printed
+ *  in main().
+ */
+
+int run(std::string endpointAddress, std::string portNumber, int leptonID) {
+
+    ImageStream *stream;
+
+#ifdef SAILBOT_DEBUG
+    std::string inputFrameDir = Paths::join(Resources::RESOURCE_DIR, "img/16bit");
+    stream = new FileSystemImageStream(inputFrameDir, "*.png");
+#else
+    stream = new ThermalCameraStream(LeptonRegistry::getLepton0());
+#endif
+
+    TCPImageServer server(*stream, endpointAddress, portNumber);
+
+    for (;;) {
+        pause();
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc < 3) {
+        std::cout << std::endl;
+        std::cout << "Usage: " << APPNAME << " [IPv4_address] [port#] [leptonID]" << std::endl;
+        std::cout << std::endl;
+        std::cout << "e.g. ./" << APPNAME << " " << "'*' 5555 0" << std::endl;
+        std::cout << std::endl;
+        std:: cout << "This binds an instance of cameraServer to any available interface "
+            "on this device, on port 5555, connecting to a Lepton object "
+            "initialized to be controlled by SPI_CS_0." << std::endl;
+        exit(0);
+    }
+
+    std::string address(argv[1]), port(argv[2]);
+    int leptonID = atoi(argv[3]);
+
+    run(address, port, leptonID);
+
+    return 0;
+}
